c9d0088bb79f14bf44b2aaa7c573f5f0a7d09abd
sihanemilyhe <she@stu.naperville203.org>
2026-05-14 11:48:54 -0500

    code

--- ColorLabs17/TestPicture17.java
+++ ColorLabs17/TestPicture17.java
@@ -77,200 +77,12 @@ public class TestPicture17
          }
     }
     }
-     big(pic4, acanvas,1280,720);
-     shrink(pic6,acanvas,2560,720);
-     acanvas.explore();
-     
-     /*
-     //makes an array of pixels
-     Pixel[] pixels;
-     Pixel[] pixels2;
-     Pixel[] pixels3;
-     Pixel[] pixels4;
-     Pixel[] pixels5;
-     Pixel[] pixels6;
-     //gets pixels from picture and assigns to pixels array
-     pixels = pic.getPixels();
-     pixels2 = pic2.getPixels();
-     pixels3 = pic3.getPixels();
-     pixels4 = pic4.getPixels();
-     pixels5 = pic5.getPixels();
-     pixels6 = pic6.getPixels();
-
-     
-     
-     
-
-     
-     Pixel spot1 = pic.getPixel(100,100);
-     Pixel spot2 = pic.getPixel(433,283);
-     Pixel ferr17 = pixels[17];
-     ferr17.setRed(240);
-     ferr17.setGreen(160);
-     ferr17.setBlue(200);
-     //spot2.setColor(Color.blue);
-     int red,green,blue;
-     pic.explore();
-     for (Pixel spot: pic3.getPixels()) {
-         red = spot.getRed();
-         red = (int)(red*0.5);
-         spot.setRed(red);
-        }
-     pic3.explore();
-     for (Pixel spot:pic.getPixels()){
-         blue = spot.getBlue();
-         blue = (255-blue);
-         spot.setBlue(blue);
-         green = spot.getGreen();
-         green = (255-green);
-         spot.setGreen(green);
-         red = spot.getRed();
-         red = (255-red);
-         spot.setRed(red);
-         
-    
-     }
-     pic.explore();
-    
-     for (Pixel spot:pic2.getPixels()){
-         blue = spot.getBlue();
-         green = spot.getGreen();
-         red = spot.getRed();
-         int grey = (blue+red+green)/3;
-         blue = (grey);
-         spot.setBlue(blue);
-         green = (grey);
-         spot.setGreen(green);
-         red = (grey);
-         spot.setRed(red);
-         }
-     pic2.explore();
-     
-     for (Pixel spot:pic4.getPixels()){
-         blue = spot.getBlue();
-         blue = (blue-80);
-         spot.setBlue(blue);
-         green = spot.getGreen();
-         green = (green-80);
-         spot.setGreen(green);
-         red = spot.getRed();
-         red = (red-80);
-         spot.setRed(red);
-         
-    
-     }
-     pic4.explore();
-     
-     for (Pixel spot:pic5.getPixels()){
-         blue = spot.getBlue();
-         blue = (blue+90);
-         spot.setBlue(blue);
-         green = spot.getGreen();
-         green = (green+90);
-         spot.setGreen(green);
-         red = spot.getRed();
-         red = (red+90);
-         spot.setRed(red);
-         
-    
-     }
-     pic5.explore();
-     
-     for (Pixel spot:pic6.getPixels()){
-         blue = spot.getBlue();
-         red = spot.getRed();
-         green = spot.getGreen();
-         if (red<=140&&red>=0) {
-            if (blue<=70&&blue>=0) {
-                if (green<=70&&green>=0) {
-            spot.setBlue((int)(blue*2.5));
-         }
-         }
-         }
-         
-         
-    
-     }
-     pic6.explore();
-         
-    }
-    
-     //how many pixels or how large array
-    //System.out.println("This is a large array"+pixels.length  );
-
-
-    /*
-        //access each index
-    System.out.println(pixels[17]);
-    //access each pixel
-    Pixel spot = ferris1.getPixel(100,100);
-    
-    System.out.println(pixels[17].getColor());
-    System.out.println(spot);
-/*
-    pixels[17].setColor(Color.blue);
-    spot.setColor(new Color(252,252,252));
-    pixels[500034].setColor(Color.blue);
-
-    ferris1.explore();
-/*
-   // loop to access indexes of array or collection
-
-    //for each loop spot  is a ?
-    for (Pixel spot : pixels)
-    System.out.println( spot );
-
-
    
- /**/
+     big(pic4, acanvas,1280,720);
+     shrink(pic6,pic6,1275,715);
 
- /**
-  * Method to clear red from picture
-  * @param none
-  * @return none
-  */
- /*
-    for (Pixel pixelObj : pixels)
-        {
-            //set the red value of the current pixel to the new value
-           
-
-        }
-    ferris1.explore();
-    
-/**/
- /**
-  * Method to reduce red from picture by a factor of n
-  * @param none
-  * @return none
-  */
-
-/*
-int value;
-final double  FACTOR = .5;
-    for (Pixel pixelObj : pixels)
-    {
-
-        //get the redvalue
-        value = pixelObj.getRed();
-        //System.out.println(value);
-
-        //decrease the red value by 50%
-        
-        //set the red value of the current pixel to the new value
-        
-
-    }
-    // use new picture when changing or it will make changes to 
-    // pic you already changed
-    ferris1.explore();
-    ferris2.explore();
-
-  /**/ 
-  /**/
-    //write/save a picture as a file
-    //ferris1.write("images/ferris11.jpg");
-
+     pic6.explore();
+     acanvas.explore();
     
 
 }//main
@@ -352,20 +164,20 @@ public static void shrink(Picture source, Picture target, int num1, int num2){
 
     //loop thru jcolumns (targetX is the starting point on the Canvas) sourceX+=2 - smaller copy every other pixel
                     //                                                  sourceX +=.5 - larger, copy every pixel twice cast as int in the getPixel & setColor
-    if (num1>0&&num2>0){
+    if (source.getWidth()>100&&source.getHeight()>100){
     for (double sourceX = 0,targetX = num1; sourceX < source.getWidth();sourceX+=2,targetX++){
         //loop thru the rows                                sourceY+=2 - smaller
         //                                                  sourceX +=.5 - larger, copy every pixel twice 
         for (double sourceY = 0,targetY = num2; sourceY < source.getHeight();sourceY+=2,targetY++){
+            System.out.println(sourceX + " " + targetX);
+            System.out.println(sourceY + " " + targetY);
             sourcePix = source.getPixel((int)sourceX, (int)sourceY);
             targetPix = target.getPixel((int)targetX, (int)targetY);
             targetPix.setColor(sourcePix.getColor());
-
-            
         
         }
     }
-    shrink(source, target, (int)num1/2, (int)num2/2);
+    shrink(source, target, (int)source.getWidth()/2, (int)source.getHeight()/2);
 }
 }
 
